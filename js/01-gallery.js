daead141bc334d3652a8b300d4049234a5a70d88
Dmitry Salyakin <d.salyakin@gmail.com>
2022-08-18 11:56:04 +0300

    Add gallery markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const makeGalleryItems = (galleryItems) =>
+  galleryItems.reduce(
+    (acc, { preview, original, description }) =>
+      acc +
+      `
+      <div class="gallery__item">
+        <a class="gallery__link" href=${original}>
+          <img
+            class="gallery__image"
+            src=${preview}
+            data-source=${original}
+            alt=${description}
+          />
+        </a>
+      </div>`,
+    ''
+  );
+
+const galleryRef = document.querySelector('.gallery');
+galleryRef.insertAdjacentHTML('beforeend', makeGalleryItems(galleryItems));
+
+
