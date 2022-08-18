2a968bbd4aad2a15e495a4557bdf093d0c28896c
Dmitry Salyakin <d.salyakin@gmail.com>
2022-08-18 16:06:47 +0300

    Add SimpleLightbox & finish Task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const makeGalleryItems = (galleryItems) =>
+  galleryItems.reduce(
+    (acc, { preview, original, description }) =>
+      acc +
+      `
+      <a class="gallery__item" href=${original}>
+        <img
+          class="gallery__image"
+          src=${preview}
+          alt=${description}
+        />
+      </a>`,
+    ''
+  );
+
+const galleryRef = document.querySelector('.gallery');
+galleryRef.insertAdjacentHTML('beforeend', makeGalleryItems(galleryItems));
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  // captionPosition: 'bottom',
+  captionDelay: 250,
+});
